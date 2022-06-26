0cdea55c04375456dfc5e625a8ebdb24970002cd
You-saku <yousaku.dev19@gmail.com>
2022-06-26 20:47:33 +0900

    add: webサーバーを立てる

---
+++ src/web/target/debug/deps/web-d81e22f481e8b7ed.d
@@ -0,0 +1,5 @@
+/usr/src/rust/src/web/target/debug/deps/web-d81e22f481e8b7ed: src/main.rs
+
+/usr/src/rust/src/web/target/debug/deps/web-d81e22f481e8b7ed.d: src/main.rs
+
+src/main.rs:
---
+++ src/web/target/debug/web.d
@@ -0,0 +1,1 @@
+/usr/src/rust/src/web/target/debug/web: /usr/src/rust/src/web/src/main.rs
